f1dc9b662a40139d382ec594f09cb9643b06f902
Felipe Huffner <felipeqh.1991@gmail.com>
2022-06-23 17:37:57 -0400

    component Header

---
+++ src/pages/Event.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/Header";
+import { Lesson } from "../components/Lesson";
+import { Sidebar } from "../components/Sidebar";
+import { Video } from "../components/Video";
+
+export function Event(){
+    return(
+    <Header />
+    )
+}
