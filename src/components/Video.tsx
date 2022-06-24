eeec137b6230da3cda71133370ccd5c674e4f290
Felipe Huffner <felipeqh.1991@gmail.com>
2022-06-23 20:04:07 -0400

    Lessons e Sidebar

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,5 +1,7 @@
 export function Video(){
     return (
-        <></>
+        <div className="flex-1">
+            Video
+        </div>
     )
 }
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -3,8 +3,14 @@ import { Lesson } from "../components/Lesson";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
-export function Event(){
-    return(
-    <Header />
-    )
+export function Event() {
+  return (
+    <div className="flex flex-col min-h-screen" >
+      <Header />
+      <main className="flex flex-1">
+      <Video />
+      <Sidebar />
+      </main>
+    </div>
+  );
 }
