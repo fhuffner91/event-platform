f2cb7c195c467bc0bdd1cac83200c3f4eadb672e
Felipe Huffner <felipeqh.1991@gmail.com>
2022-06-24 17:49:32 -0400

    player and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Event } from "./pages/Event"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./components/Router"
+import { client } from "./lib/apollo"
 
 
 function App(){
   return(
-    <Event />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+      <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
